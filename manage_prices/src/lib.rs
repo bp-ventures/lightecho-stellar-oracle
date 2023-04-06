9a5c983c8e6fd1e4eedc245f3b2847f0b139aba3
qaisark <qaisarkasana6@gmail.com>
2023-04-06 17:35:19 +0500

    update

--- manage_prices/src/lib.rs
+++ manage_prices/src/lib.rs
@@ -16,15 +16,38 @@ pub struct PriceUpdate;
 
 #[contractimpl]
 impl PriceUpdate {
-    // setting price in oracle
-    pub fn set_price(e: &Env, price: &Price) {
-        e.storage().set(&DataKey::Price, price);
+    pub fn create(e: Env, seller: AccountId, sell_price: u32, buy_price: u32) {
+        if e.storage().has(&DataKey::Price) {
+            panic!("price is already created");
+        }
+        let price = Price {
+            seller,
+            sell_price,
+            buy_price,
+        };
+        e.storage().set(&DataKey::Price, &price);
     }
 
-    // getting price from oracle
-    pub fn get_price(e: Env) -> Price {
-        e.storage().get_unchecked(&DataKey::Price).unwrap()
+    pub fn update(e: Env, seller: AccountId, sell_price: u32, buy_price: u32) {
+        if !e.storage().has(&DataKey::Price) {
+            panic!("price is not created");
+        }
+        let price = Price {
+            seller,
+            sell_price,
+            buy_price,
+        };
+        e.storage().set(&DataKey::Price, &price);
     }
+}
+// getting price from oracle
+pub fn get_price(e: &Env) -> Price {
+    e.storage().get_unchecked(&DataKey::Price).unwrap()
+}
+
+// setting price in oracle
+pub fn set_price(e: &Env, price: &Price) {
+    e.storage().set(&DataKey::Price, price);
 }
 
 mod test;
