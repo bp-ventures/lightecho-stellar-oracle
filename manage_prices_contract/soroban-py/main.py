4d5858df969b7bf4668f019a14668e1ff1e5f7ce
qaisark <qaisarkasana6@gmail.com>
2023-04-12 23:58:56 +0500

    added prompts and improvements

--- manage_prices_contract/soroban-py/main.py
+++ manage_prices_contract/soroban-py/main.py
@@ -4,6 +4,8 @@ if __name__ == "__main__":
     print("------------------------------------------------")
     print("-------------Soroban Prices Oracle--------------")
     print("------------------------------------------------")
+    print("Make sure contract id is valid on the FUTURENET network")
+    contract_id = input("Enter contract id: ")
 
     print("1 - Invoke create function")
     print("2 - Invoke update function")
@@ -15,6 +17,6 @@ if __name__ == "__main__":
     if prompt == 0:
         exit()
     elif prompt == 1 or prompt == 2 or prompt == 3 or prompt == 4:
-        Soroban.invoke_contract(prompt)
+        Soroban.invoke_contract(prompt, contract_id)
     else:
         print("Invalid choice")
--- manage_prices_contract/soroban-py/soroban.py
+++ manage_prices_contract/soroban-py/soroban.py
@@ -2,26 +2,29 @@ import time
 import config
 from stellar_sdk import Asset, Keypair, TransactionBuilder
 from stellar_sdk import xdr as stellar_xdr
-from stellar_sdk.soroban import types, ContractAuth, AuthorizedInvocation, SorobanServer
+from stellar_sdk.soroban import types, SorobanServer
 from stellar_sdk.soroban.soroban_rpc import GetTransactionStatus
-from stellar_sdk.soroban.types import Uint32, Uint128, Address, Symbol
+from stellar_sdk.soroban.types import Uint32, Address
 
 
 class Soroban:
-    def invoke_contract(prompt):
+    def invoke_contract(prompt, contract_id):
         if prompt == 1:
+            # Sample args for create function
             args = [
                 Address("GALGFV6YVKMVAWHK6QA7GCC67VKBW73A3PB5IKZGKT5ID5AGK4S3Y7GX"),
                 Uint32(1),
                 Uint32(1),
             ]
         elif prompt == 2:
+            # Sample args for update function
             args = [
                 Address("GALGFV6YVKMVAWHK6QA7GCC67VKBW73A3PB5IKZGKT5ID5AGK4S3Y7GX"),
                 Uint32(5),
                 Uint32(5),
             ]
         elif prompt == 3 or prompt == 4:
+            # Empty args for get and delete functions
             args = []
 
         Soroban.invoke(
@@ -33,20 +36,19 @@ class Soroban:
             else "get"
             if prompt == 3
             else "delete",
+            contract_id=contract_id,
         )
 
-    def invoke(args, fun_name):
-        secret = "SCNLUY7SFXJYVIULV66V2OQHNB4XDWYFGNNCN5YBBC3MZT5XN4X7IJP6"
-
+    def invoke(args, fun_name, contract_id):
         # This contract id should be a valid contract id on the futurenet network.
-        contract_id = "71bba71125a417479f6f3069ed336e9e5246132f624a738a08250c156aad3f92"
+        # contract_id = "71bba71125a417479f6f3069ed336e9e5246132f624a738a08250c156aad3f92"
 
         try:
-            kp = Keypair.from_secret(secret)
+            kp = Keypair.from_secret(config.secret)
             soroban_server = SorobanServer(config.rpc_server_url)
             source = soroban_server.load_account(kp.public_key)
 
-            # Let's build a transaction that invokes the function.
+            # build a transaction that invokes the function
             tx = (
                 TransactionBuilder(source, config.network_passphrase)
                 .set_timeout(300)
@@ -88,13 +90,11 @@ class Soroban:
                     get_transaction_data.result_meta_xdr
                 )
                 result = transaction_meta.v3.tx_result.result.results[0].tr.invoke_host_function_result.success  # type: ignore
-                # output = [x.sym.sc_symbol.decode() for x in result.vec.sc_vec]  # type: ignore
-                # print(f"transaction result: {output}")
 
-                # Comment if you want to invoke get function
                 if fun_name == "create" or fun_name == "update" or fun_name == "delete":
                     print(f"transaction result: {result}")
                 else:
+                    # No default decode function available right now, decoding responses based on my parameters
                     struct = types.Struct.from_xdr_sc_val(result)
                     print(
                         f"key: {struct.fields[0].key}, value: {types.Uint32.from_xdr_sc_val(struct.fields[0].value).value}"
@@ -105,18 +105,6 @@ class Soroban:
                     print(
                         f"key: {struct.fields[2].key}, value: {types.Address.from_xdr_sc_val(struct.fields[2].value).address}"
                     )
-
-                # Uncomment if you want to invoke get function
-                # struct = types.Struct.from_xdr_sc_val(result)
-                # print(
-                #     f"key: {struct.fields[0].key}, value: {types.Uint32.from_xdr_sc_val(struct.fields[0].value).value}"
-                # )
-                # print(
-                #     f"key: {struct.fields[1].key}, value: {types.Uint32.from_xdr_sc_val(struct.fields[1].value).value}"
-                # )
-                # print(
-                #     f"key: {struct.fields[2].key}, value: {types.Address.from_xdr_sc_val(struct.fields[2].value).address}"
-                # )
         except Exception as e:
             print(f"Error: {e}")
             return
