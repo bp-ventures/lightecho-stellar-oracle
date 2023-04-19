cfbd0089a96078b20c11e634248034e2e2e60c59
qaisark <qaisarkasana6@gmail.com>
2023-04-19 19:41:48 +0500

    struct & contract footprint

--- xlm_payment_contract/src/lib.rs
+++ xlm_payment_contract/src/lib.rs
@@ -1,1 +1,32 @@
+#![no_std]
 
+use soroban_sdk::{contractimpl, contracttype, Address, BytesN, Env};
+
+mod token {
+    soroban_sdk::contractimport!(file = "../utils/token/soroban_token_spec.wasm");
+}
+
+#[derive(Clone)]
+#[contracttype]
+
+pub enum DataKey {
+    Payment,
+}
+
+#[derive(Clone)]
+#[contracttype]
+
+pub struct Payment {
+    pub from: Address,
+    pub to: Address,
+    pub amount: i128,
+    pub token_id: BytesN<32>,
+}
+
+pub struct PaymentContract;
+
+#[contractimpl]
+impl PaymentContract {
+    // send payment
+    pub fn send(e: Env, from: Address, to: Address, amount: i128, token_id: BytesN<32>) {}
+}
