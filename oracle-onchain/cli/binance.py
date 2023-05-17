4857b1037edd60241980f51c611285c64c9c2608
Yuri E <yuriescl@bpventures.us>
2023-05-17 00:29:34 +0000

    fixed binance

--- oracle-onchain/cli/binance.py
+++ oracle-onchain/cli/binance.py
@@ -16,10 +16,10 @@ r = requests.get(u)
 df = pd.DataFrame(r.json())
 df2 = df[df.symbol.isin(filter_symbols)]
 print("start binance")
-cmd = (pathlib.Path(__file__).parent / "cli.py").resolve()
+cli_dir = pathlib.Path(__file__).parent.resolve()
 for index, row in df2.iterrows():
     if row["symbol"] in ["XLMUSDT"]:
-        out = check_output([cmd, "set-rate", "USD", " ", "1", row["lastPrice"]])
+        out = check_output(["./cli.py", "set-rate", "USD", " ", "1", row["lastPrice"]], cwd=cli_dir)
         print(out)
         # proc = subprocess.Popen([cmd, "set-rate", "USDC", "GBBD47IF6LWK7P7MDEVSCWR7DPUWV3NY3DTQEVFL4NAT4AQH3ZLLFLA5", row['lastPrice'], "1"],
         #    stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
