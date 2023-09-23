1093c8cee2448d5656a6aa68e435beeefb70b6dd
Yuri E. <yuriescl@gmail.com>
2023-09-23 07:52:27 -0300

    fixes

--- oracle-onchain/sep40/examples/price_up_down/scripts/deploy.sh
+++ oracle-onchain/sep40/examples/price_up_down/scripts/deploy.sh
@@ -7,6 +7,22 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <network>"
+  exit 1
+fi
+NETWORK="$1"
+if [ "$NETWORK" == "testnet" ]; then
+  RPC_URL=https://soroban-testnet.stellar.org
+  NETWORK_PASSPHRASE='Test SDF Network ; September 2015'
+elif [ "$NETWORK" == "futurenet" ]; then
+  RPC_URL=https://rpc-futurenet.stellar.org:443
+  NETWORK_PASSPHRASE='Test SDF Future Network ; October 2022'
+else
+  echo "Invalid network. Please provide either 'testnet' or 'futurenet'."
+  exit 1
+fi
+
 if ! command -v poetry &> /dev/null
 then
   >&2 printf "${RED}poetry command not found. Visit https://python-poetry.org/ for installation instructions.${NC}\n"
@@ -24,12 +40,12 @@ set -e
 echo "➤ Building contract"
 make
 
-echo "➤ Deploying contract to Futurenet"
+echo "➤ Deploying contract to $NETWORK"
 contract_id=$(soroban contract deploy \
     --wasm target/wasm32-unknown-unknown/release/price_up_down.wasm \
     --source "$SOURCE_SECRET" \
-    --rpc-url https://rpc-futurenet.stellar.org:443 \
-    --network-passphrase 'Test SDF Future Network ; October 2022')
+    --rpc-url "$RPC_URL" \
+    --network-passphrase "$NETWORK_PASSPHRASE")
 
 if [ -z "$contract_id" ]; then
     printf "${RED}Failed to deploy contract${NC}\n"
