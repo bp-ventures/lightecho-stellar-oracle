0d17e55fabf592c3875535be4d2fb6411552fe34
Yuri E <yuriescl@gmail.com>
2023-11-03 13:18:43 -0300

    fixes

--- oracle-onchain/sep40/cli/scripts/feed_all_from_db.py
+++ oracle-onchain/sep40/cli/scripts/feed_all_from_db.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env -S poetry run python
 import sqlite3
 import logging
 import importlib.util
--- oracle-onchain/sep40/cli/scripts/feed_all_from_db.sh
+++ oracle-onchain/sep40/cli/scripts/feed_all_from_db.sh
@@ -3,4 +3,4 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR" || exit
 
-source ~/.bashrc && ./feed_all_from_db.py
+source ~/.bashrc && poetry run feed_all_from_db.py
