261d65565757d498b116cee48cf32cb69736d945
Yuri E <yuriescl@gmail.com>
2023-11-03 13:20:38 -0300

    fixes

--- oracle-onchain/sep40/cli/scripts/feed_all_from_db.sh
+++ oracle-onchain/sep40/cli/scripts/feed_all_from_db.sh
@@ -3,4 +3,4 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR" || exit
 
-~/.local/bin/poetry run feed_all_from_db.py
+~/.local/bin/poetry run python feed_all_from_db.py
