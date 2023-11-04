a34c3d166fae86b1761808c60eff7352234784bf
Yuri E <yuriescl@gmail.com>
2023-11-04 09:04:28 -0300

    bump_instance script

---
+++ oracle-onchain/sep40/cli/scripts/bump_instance.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd "$SCRIPT_DIR" || exit
+cd ..
+~/.local/bin/poetry run python cli.py oracle bump_instance
