631f0a82f46a7f5e6d1410bc349ee25435f98962
Yuri E <yuriescl@gmail.com>
2023-11-05 10:11:23 -0300

    fixes

--- oracle-onchain/sep40/cli/scripts/bump_instance.sh
+++ oracle-onchain/sep40/cli/scripts/bump_instance.sh
@@ -3,4 +3,4 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR" || exit
 cd ..
-~/.local/bin/poetry run python cli.py oracle bump_instance
+~/.local/bin/poetry run python cli.py oracle bump_instance || ./send_email_failed_bump_instance.sh
--- oracle-onchain/sep40/cli/scripts/feed_all_from_db.sh
+++ oracle-onchain/sep40/cli/scripts/feed_all_from_db.sh
@@ -3,4 +3,4 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR" || exit
 
-~/.local/bin/poetry run python feed_all_from_db.py
+~/.local/bin/poetry run python feed_all_from_db.py || ./send_email_failed_feed_prices.sh
