4431333af8bf38be47d507d82a942182f39f93c0
Yuri E <yuriescl@gmail.com>
2023-11-06 07:40:04 -0300

    email new lines

--- oracle-onchain/sep40/cli/scripts/send_email_failed_bump_instance.sh
+++ oracle-onchain/sep40/cli/scripts/send_email_failed_bump_instance.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR" || exit
-echo "Failed to bump Oracle instance. Use these commands to see the logs:\nsudo machinectl shell lightecho-stellar-oracle@\njournalctl --user -u bump_instance\n" | mutt  -s "Oracle bump failed" -- support@bpventures.us
+echo -e "Failed to bump Oracle instance. Use these commands to see the logs:\nsudo machinectl shell lightecho-stellar-oracle@\njournalctl --user -u bump_instance\n" | mutt  -s "Oracle bump failed" -- support@bpventures.us
--- oracle-onchain/sep40/cli/scripts/send_email_failed_feed_prices.sh
+++ oracle-onchain/sep40/cli/scripts/send_email_failed_feed_prices.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR" || exit
-echo "Failed to feed prices into Oracle. Use these commands to see the logs:\nsudo machinectl shell lightecho-stellar-oracle@\njournalctl --user -u feed_all_from_db\n" | mutt  -s "Oracle feed failed" -- support@bpventures.us
+echo -e "Failed to feed prices into Oracle. Use these commands to see the logs:\nsudo machinectl shell lightecho-stellar-oracle@\njournalctl --user -u feed_all_from_db\n" | mutt  -s "Oracle feed failed" -- support@bpventures.us
